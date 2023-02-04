d4e0294190d66f5aa36759ae79d2bdfcda121028
brennenfire <barbustefan2002@gmail.com>
2023-02-05 01:05:51 +0200

    initial commit with basic player movement

---
+++ platformgamejam/gamejamplatformer/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float decceleration = 1f;
+    [SerializeField] float acceleration = 1f;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float jumpVelocity = 1f;
+    [SerializeField] Transform feet;
+
+    new Rigidbody2D rigidbody;
+    string readHorizontal;
+    string jumpButton;
+    string attackButton;
+    float horizontal;
+    bool isGrounded;
+    int layerMask;
+    int jumpsRemaining = 1;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        readHorizontal = $"PHorizontal";
+        jumpButton = $"PJump";
+        layerMask = LayerMask.GetMask("Default");
+    }
+
+    void Update()
+    {
+        UpdateIsGrounded();
+        ReadHorizontalInput();
+        MoveHorizontal();
+        if (ShouldStartJump() && isGrounded)
+        {
+            Jump();
+        }
+        if (isGrounded)
+        {
+            Debug.Log("is grounded");
+            jumpsRemaining = 1;
+        }
+      
+    }
+
+    void Jump()
+    {
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpVelocity);
+        jumpsRemaining--;
+    }
+
+    bool ShouldStartJump()
+    {
+        return Input.GetButtonDown(jumpButton) && jumpsRemaining > 0;
+    }
+
+    void ReadHorizontalInput()
+    {
+        horizontal = Input.GetAxis(readHorizontal) * speed;
+    }
+    void MoveHorizontal()
+    {
+        float smoothnessMultiplier = horizontal == 0 ? decceleration : acceleration;
+        float newHorizontal = Mathf.Lerp(rigidbody.velocity.x, horizontal * speed, Time.deltaTime * smoothnessMultiplier);
+        rigidbody.velocity = new Vector2(newHorizontal, rigidbody.velocity.y);
+    }
+    void UpdateIsGrounded()
+    {
+        var hit = Physics2D.OverlapCircle(feet.position, 0.1f, layerMask);
+        isGrounded = hit != null;
+    }
+}
