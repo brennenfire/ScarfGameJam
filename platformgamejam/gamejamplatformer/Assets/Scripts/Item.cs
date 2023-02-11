d3967557b05491def480a493e88bff7ba372074f
brennenfire <barbustefan2002@gmail.com>
2023-02-12 01:55:06 +0200

    game jam version

--- platformgamejam/gamejamplatformer/Assets/Scripts/Item.cs
+++ platformgamejam/gamejamplatformer/Assets/Scripts/Item.cs
@@ -8,35 +8,62 @@ using UnityEngine.UI;
 public class Item : MonoBehaviour, IInteract
 {
     [SerializeField] bool onlyExamine = true;
+    [SerializeField] bool isPickup = false;
     [SerializeField] TMP_Text textBox;
     [SerializeField] Image image;
     [SerializeField] List<string> text;
     bool canExamine = true;
+    bool hasRope = false;
+    bool hasBigRope = false;
     int textCounter = 0;
 
+    // static script with array of game objects for equipment
+    
     void Update()
     {
-        foreach(var obj in text)
-        {
-            textCounter++;
-        }
+
     }
 
     public void Interact()
     {
         if (onlyExamine && canExamine)
         {
+            /*
+            if(textCounter > text.Count)
+            {
+                return;
+            }
+            */
             canExamine = false;
             image.enabled = true;
-            textBox.text = text[text.Count - textCounter++];
+            textBox.text = text[0];
+            StartCoroutine(TurnOffBox());
+        }
+
+        if(isPickup)
+        {
+            canExamine = false;
+            image.enabled = true;
+            textBox.text = text[0];
             StartCoroutine(TurnOffBox());
         }
     }
 
     IEnumerator TurnOffBox()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(2.5f);
+        textBox.text = text[1];
+        yield return new WaitForSeconds(2.5f);
         image.enabled = false;
+        if(isPickup)
+        {
+            gameObject.SetActive(false);
+            hasRope = true;
+            if(hasRope)
+            {
+                hasBigRope = true;
+            }
+        }
         textBox.text = "";
         canExamine = true;
     }
---
+++ platformgamejam/gamejamplatformer/Assets/Scripts/RopePole.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RopePole : MonoBehaviour, IInteract
+{
+    [SerializeField] GameObject smallRope;
+    [SerializeField] GameObject bigRope;
+    [SerializeField] TMP_Text textBox;
+    [SerializeField] TMP_Text endText;
+    [SerializeField] Image image;
+    [SerializeField] Animator animator;
+
+    public void Interact()
+    {
+        if(smallRope.activeSelf == false && bigRope.activeSelf == true)
+        {
+            textBox.text = "This rope is too short.";
+            image.enabled = true;
+            StartCoroutine(TurnOffBox());
+        }
+        
+        if(bigRope.activeSelf == false) 
+        {
+            animator.SetTrigger("End");
+            endText.text = "Thank for you playing this demo.";
+            StartCoroutine(TurnOffGame());
+        }
+        
+        else
+        {
+            textBox.text = "It's a rope post.";
+            image.enabled = true;
+            StartCoroutine(TurnOffBox());
+        }
+        
+    }
+
+    IEnumerator TurnOffBox()
+    {
+        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(2.5f);
+        image.enabled = false;
+        textBox.text = "";
+    }
+
+    IEnumerator TurnOffGame()
+    {
+        yield return new WaitForSeconds(7f);
+        Application.Quit();
+    }
+}
